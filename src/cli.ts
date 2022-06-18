88feef07a3a341ed2109b4b16cc8cc23fc8a7294
Lasse Koskela <lasse.koskela@gmail.com>
2022-06-19 01:08:03 +0300

    Make `src/cli.ts` executable (with `npm link` and `"bin":"build/cli.js"` in `package.json`)

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,13 @@
+#!/usr/bin/env node
+
 const process = require('process')
 import { parseSpdxExpression } from './parser'
 
 process.argv.slice(2).forEach((arg:string) => {
-    const ast = parseSpdxExpression(arg)
-    console.log(JSON.stringify(ast, null, 2))
+    try {
+        const ast = parseSpdxExpression(arg)
+        console.log(JSON.stringify(ast, null, 2))
+    } catch (error: any) {
+        console.log(JSON.stringify({ error: error.message }, null, 2))
+    }
 })
