c74e192bcddc3c4b60a1fae28f9dedab05b4c2f9
Lasse Koskela <lasse.koskela@gmail.com>
2022-07-08 13:09:48 +0300

    Export {ParsedSpdxExpression, ConjunctionInfo, LicenseInfo, LicenseRef} from the main module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { parse } from './parser'
+import { parse, ParsedSpdxExpression, ConjunctionInfo, LicenseInfo, LicenseRef } from './parser'
 import normalize from './normalizer'
 import validate from './validator'
 
-export { parse, normalize, validate }
+export { parse, normalize, validate, ParsedSpdxExpression, ConjunctionInfo, LicenseInfo, LicenseRef }
 export default parse
